3717366528091df4897c11273294eb1ad45044ea
davidd706 <130115461+davidd706@users.noreply.github.com>
2023-06-02 15:05:23 -0700

    Create enroll_student.py

---
+++ enroll_student.py
@@ -0,0 +1,43 @@
+from data_file import constant_students
+
+def enroll_student():
+    while True:
+        first_name = input("Enter New Student Information Below\nFirst Name: ")
+        last_name = input("Last Name: ")
+        sex = input("Gender: ")
+        dob = input("Date-Of-Birth: ")
+        while True:
+            schedule = []
+            grades = []
+            while True:
+                student_class = input("Enter Classes (Input 'X' When Finalized): ")
+                if student_class == "X":
+                    break
+                schedule.append(student_class)
+            while True:
+                class_grade = input("Enter Grades (Input 'X' When Finalized): ")
+                if class_grade == "X":
+                    break
+                grades.append(class_grade)
+            if len(schedule) == len(grades):
+                break
+            else:
+                print("Invalid Attempt\nReason: Class And Grade Quantites Are Not Equal\nPlease Try Again")
+                pass
+        user_decision = input("Please Verify New Student Information\nFinalize New Student Profile? (y/n): ")
+        if user_decision == "n":
+            pass
+        elif user_decision == "y":
+            break
+    student_profile = {
+        "first_name": first_name,
+        "last_name": last_name,
+        "sex": sex,
+        "DOB": dob,
+        "schedule": schedule,
+        "grades": grades,
+        "active": True
+    }
+    constant_students.append(student_profile)
+
+enroll_student()
