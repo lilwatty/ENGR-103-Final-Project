e8b4e09a004cdbe96701042b5e54f98a23db3b9b
davidd706 <130115461+davidd706@users.noreply.github.com>
2023-06-03 21:41:19 -0700

    Update enroll_student.py

--- enroll_student.py
+++ enroll_student.py
@@ -1,44 +1,15 @@
 from local_data.data import staff_registry, student_registry
 from generate_id import generate_id
 
-# Max length of a function is 20 lines (not including formating components)
-# Divide the logic up onto separate function
-# Also, it is generally better practice to limit yourself to no more than one nested loop.
-# At that point you need to call a function
-
-# Also, the schedule length is not variable, it needs to be no more than 6.
-# You may consider for "i in range(6)" Plus, the students won't have grades when they are
-# First signed up for the class so the grade_match part isn't applicable. Do however make a
-# Blank list of strings "_" would be fine.
-
 
 def enroll_student():
     while True:
-        first_name = input("Enter New Student Information Below\nFirst Name: ")
+        first_name = input(
+            "Enter New Student Information Below\nFirst Name: ")
         last_name = input("Last Name: ")
         sex = input("Gender: ")
         dob = input("Date-Of-Birth: ")
-        while True:
-            schedule = []
-            grades = []
-            while True:
-                student_class = input(
-                    "Enter Classes (Input 'X' When Finalized): ")
-                if student_class == "X":
-                    break
-                schedule.append(student_class)
-            while True:
-                class_grade = input(
-                    "Enter Grades (Input 'X' When Finalized): ")
-                if class_grade == "X":
-                    break
-                grades.append(class_grade)
-            if len(schedule) == len(grades):
-                break
-            else:
-                print(
-                    "Invalid Attempt\nReason: Class And Grade Quantites Are Not Equal\nPlease Try Again")
-                pass
+        schedule, grades = create_schedule()
         user_decision = input(
             "Please Verify New Student Information\nFinalize New Student Profile? (y/n): ")
         if user_decision == "n":
@@ -53,9 +24,25 @@ def enroll_student():
         "DOB": dob,
         "schedule": schedule,
         "grades": grades,
-        "active": True
+        "active": True,
     }
     student_registry.append(student_profile)
+
+
+def create_schedule():
+    schedule = []
+    grades = []
+    while True:
+        if len(schedule) == 6:
+            break
+        student_class = input(
+            "Enter Classes (Input 'X' When Finalized): ")
+        if student_class == "X":
+            break
+        schedule.append(student_class)
+    for i in range(len(schedule)):
+        grades.append("_")
+    return schedule, grades
 
 
 enroll_student()
