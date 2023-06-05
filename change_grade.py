590814c2012a14e52736fb2cd453ff05e692dca9
lilwatty <watsoada@oregonstate.edu>
2023-06-04 19:57:51 -0700

    Tying it all together

---
+++ change_grade.py
@@ -0,0 +1,23 @@
+from local_data.data import student_registry
+
+# Your code needs to take in the user_data["subject"] as a parameter and only allow the teacher to change
+# the grade for their class. Accessing the student by ID is good. Keep in mind, the teacher would have
+# looked them up already in the search student file
+
+
+def change_student_grade(student_id, new_grade):
+    # Check if the student ID exists in the registry
+    if student_id not in student_registry:
+        print("Student ID not found.")
+        return
+
+    # Update the student's grade
+    student_registry[student_id]['grade'] = new_grade
+    print("Grade updated successfully.")
+
+
+# User inputs
+student_id = input("Enter the student ID: ")
+new_grade = input("Enter the new grade: ")
+
+change_student_grade(student_id, new_grade)
