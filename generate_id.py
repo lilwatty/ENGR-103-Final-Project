2329d1a55b6cf23593be150ff2ec76f7fc2ee271
davidd706 <130115461+davidd706@users.noreply.github.com>
2023-06-07 19:00:06 -0700

    now creates unique ids, also changed id format from 934000000 to 934-000-000

--- generate_id.py
+++ generate_id.py
@@ -1,8 +1,13 @@
 import random
+from local_data.data import student_registry
 
 def generate_id():
-    randnums = str(random.randint(100000,999999))
-    id = "934" + randnums
+    num_set1 = str(random.randint(100,999))
+    num_set2 = str(random.randint(100, 999))     
+    id = "934-" + num_set1 + "-" + num_set2
+    for i in student_registry:
+        if i["ID"] == id:
+            generate_id()
     return id
-    
+      
 id = generate_id()
