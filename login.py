94aa7cf5d953975cd140a24b2a6cf675a717d87e
lilwatty <watsoada@oregonstate.edu>
2023-06-16 23:29:00 -0700

    Update login.py

--- login.py
+++ login.py
@@ -1,4 +1,14 @@
-def login(user_data):
+#######################################################################
+# Function: Login
+# Description: Accesses teacher database and checks to see if user enter in the proper login cridentials.
+# Parameters: Staff registry
+# Return values: Specific dictionary and changes logged in to be True.
+# Pre-Conditions: The staff registry is in the form a list of dictionaries.
+# Post-Conditions: None
+#######################################################################
+
+
+def login(staff_registry):
 
     login_attempts = 0
     max_attempts = 3
@@ -8,14 +18,16 @@ def login(user_data):
         user_attempt = input("Username: ")
         password_attempt = input("Password: ")
 
-        for i in user_data:
+        # Checks to see if the username matches on in the registry.
+        for i in staff_registry:
             if user_attempt == i["username"]:
+                # Does password match username?
                 if password_attempt == i["password"]:
                     logged_in = True
                     return i, logged_in
 
         else:
-            login_attempts += 1
+            login_attempts += 1  # Maintains security
             print(
                 f'Incorrect Credentials, {max_attempts - login_attempts} attempts left')
     print("Please try again later.")
