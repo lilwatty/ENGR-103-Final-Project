55a80491684a3859fb1357f8fc4c4c12c40c1441
LykanHypersport77 <134582589+LykanHypersport77@users.noreply.github.com>
2023-06-16 17:44:52 -0700

    fully functional and looks decent

--- report_card_generator.py
+++ report_card_generator.py
@@ -1,16 +1,7 @@
 from tkinter import Tk, Label, Frame
-from local_data.data import student_registry
-from login import user_data
-
-"""
-The get_student_by_id isn't necessary, this function will take in the entire dictionary of student information as the input.
-
-"""
 
 
-def display_report_card(student_id):
-    # Get the student details from the student registry
-    student = student_registry.get_student_by_id(student_id)
+def display_report_card(student):
 
     # Create a Tkinter window
     window = Tk()
@@ -22,22 +13,22 @@ def display_report_card(student_id):
 
     # Create labels to display the student's information
     Label(frame, text="Name:").grid(row=0, column=0)
-    Label(frame, text=student.name).grid(row=0, column=1)
+    Label(frame, text=(student['first_name'] +
+          student['\nlast_name'])).grid(row=0, column=1)
 
     Label(frame, text="ID:").grid(row=1, column=0)
-    Label(frame, text=student.id).grid(row=1, column=1)
+    Label(frame, text=student['ID']).grid(row=1, column=1)
 
-    Label(frame, text="Grade:").grid(row=2, column=0)
-    Label(frame, text=student.grade).grid(row=2, column=1)
+    Label(frame, text="Classes:").grid(row=2, column=0)
+    schedule = student['schedule']
+    for i, schedule in enumerate(schedule):
+            Label(frame, text=schedule).grid(row=2 + 1, column=0)
+
+    # Display the grades in separate rows
+    Label(frame, text="Grades:").grid(row=2, column=1)
+    grades = student['grades']
+    for i, grade in enumerate(grades):
+        Label(frame, text=grade).grid(row=2 + i, column=1)
 
     # Display the window
     window.mainloop()
-
-
-def main():
-    student_id = input("Enter student ID: ")
-    display_report_card(student_id)
-
-
-if __name__ == "__main__":
-    main()
