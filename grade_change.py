10a94e74f229dead9d50a4bef667d3c75ded1381
lilwatty <watsoada@oregonstate.edu>
2023-06-16 23:14:46 -0700

    Update grade_change.py

--- grade_change.py
+++ grade_change.py
@@ -1,18 +1,24 @@
-
-def display_grade():
-
-    pass
+#######################################################################
+# Function: Grade Change
+# Description: Teacher specific fuction that allows teacher to edit their student's grades.
+# Parameters: student_schedule, grades, subject
+# Return values: Returns a list of updated grades.
+# Pre-Conditions: The user has successfully signed in and has the cridentials of a teacher.
+# Post-Conditions: The student dictionary is updated with the new grade list.
+#######################################################################
 
 
 def grade_change(student_schedule, grades, subject):
 
-    # First check to see if the student is in the teacher's class
     student_schedule = list(student_schedule)
-
+    # Verify that teacher has student in their class
     if student_schedule.count(subject) == 0:
+
         print(f"This student is not enrolled in your course: {subject}")
         return
+
     else:
+        # Index used in the idit function
         period = student_schedule.index(subject)
 
         print(f'\n{subject}: {grades[period]}')
@@ -20,38 +26,26 @@ def grade_change(student_schedule, grades, subject):
         decision = input("Enter Command:\n 1 - Change Grade\n 2 - Cancel\n")
         if decision == "1":
             new_grade = input("Enter New Grade: ")
-            updated_grades = edit(new_grade, period, grades)
+            updated_grades = edit_list(new_grade, period, grades)
             return updated_grades
 
         return grades
 
+#######################################################################
+# Function: Edit List
+# Description:
+# Parameters:
+# Return values:
+# Pre-Conditions:
+# Post-Conditions:
+#######################################################################
+
 
 def edit(new_grade, period, list_grades):
+
     new_list = list(list_grades).copy()
     new_list.pop(period)
     new_list.insert(period, new_grade)
+
     print("Grade successfully changed.")
     return new_list
-
-
-'''
-schedule = [
-    "English Literature",
-    "Biology",
-    "Chemistry",
-    "Computer Science",
-    "Calculus",
-    "Physics"
-]
-grades = [
-    "A",
-    "A",
-    "B+",
-    "A",
-    "A-",
-    "B+"
-]
-subject = 'Physics'
-
-grade_change(schedule, grades, subject)
-'''
